68b446497b20d6a789a71b6182979425779323f0
Bobby Ekaro <iambobbymax@gmail.com>
2022-09-24 14:36:10 +0100

    json force

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('products/public', 'ProductController@display');
-Route::get('tags/public', 'PublicAccessController@tags');
-Route::get('classifications/public', 'PublicAccessController@classifications');
-Route::get('categories/public', 'PublicAccessController@categories');
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    Route::get('products/public', 'ProductController@display');
+    Route::get('tags/public', 'PublicAccessController@tags');
+    Route::get('classifications/public', 'PublicAccessController@classifications');
+    Route::get('categories/public', 'PublicAccessController@categories');
+});
 
 Auth::routes();
 
